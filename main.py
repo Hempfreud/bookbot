d659f19642e8e1524635b7263efa1333c387962d
Hempfreud <antoniocarooca@gmail.com>
2025-12-26 19:28:57 +0100

    save

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ def get_book_text(file_path):
         file_content = file.read()
         return file_content
 
-from stats import total_words
+from stats import total_words, character_count, sort_chars
 
 def main():
     book = get_book_text("./books/frankenstein.txt")
-    print(f"Found {total_words(book)} total words")
+    print(f"""============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {total_words(book)} total words
+--------- Character Count -------""")
 
 main()
--- stats.py
+++ stats.py
@@ -4,5 +4,21 @@ def total_words(file_content):
     return words_count
 
 def character_count(file_content):
-    file_content.lower()
-    characters
+    file_content = file_content.lower()
+    char_count = {}
+    for c in file_content:
+        if c in char_count:
+            char_count[c] += 1
+        else:
+            char_count[c] = 1
+    return char_count
+
+def sort_chars(char_count):
+    list_dict = []
+    for char, count in char_count.items():
+        new_dict = {"char": char, "num": count}
+        list_dict.append(new_dict)
+    def sort_on_num(dict):
+        return dict["num"]
+    list_dict.sort(reverse=True, key=sort_on_num)
+    return list_dict
